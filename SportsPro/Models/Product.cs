5a4e43c3b80448c6a08defc50b9860120d95710c
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-01-15 06:19:45 -0600

    Fix Product model whitespace.

--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -6,20 +6,20 @@ namespace SportsPro.Models
 {
     public class Product
     {
-		public int ProductID { get; set; }
+        public int ProductID { get; set; }
 
-		[Required(ErrorMessage = "Please enter a product code.")]
-		public string ProductCode { get; set; }
+        [Required(ErrorMessage = "Please enter a product code.")]
+        public string ProductCode { get; set; }
 
-		[Required(ErrorMessage = "Please enter a name.")]
-		public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
 
-		[Required(ErrorMessage = "Please enter a yearly price.")]
-		[Range(0.01, 1000000, ErrorMessage = "Please enter a yearly price between $0.01 and $1,000,000.")]
-		[Column(TypeName = "decimal(8,2)")]
-		public decimal? YearlyPrice { get; set; } = 0;
+        [Required(ErrorMessage = "Please enter a yearly price.")]
+        [Range(0.01, 1000000, ErrorMessage = "Please enter a yearly price between $0.01 and $1,000,000.")]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal? YearlyPrice { get; set; } = 0;
 
-		[Required(ErrorMessage = "Please enter a release date.")]
-		public DateTime ReleaseDate { get; set; } = DateTime.Now;
-	}
+        [Required(ErrorMessage = "Please enter a release date.")]
+        public DateTime ReleaseDate { get; set; } = DateTime.Now;
+    }
 }
