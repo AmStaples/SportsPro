cd97526ab8029b98f3fafbc893a96ff5a1f49e24
DewJSD <jeffreywdew@gmail.com>
2025-01-21 06:19:18 -0600

    moved Migration to gitignore and added Technician CRUD with validation

---
+++ SportsPro/Controllers/TechnicianController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private SportsProContext context { get; set; }
+
+        public TechnicianController(SportsProContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public ViewResult List()
+        {
+            var technicians = context.Technicians.OrderBy(t => t.Name).ToList();
+            return View(technicians);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            var technician = new Technician();
+            ViewBag.Mode = "Add";
+            return View("Edit", technician);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Technicians.Add(technician);
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Mode = "Add";
+                return View("Edit", technician);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var technician = context.Technicians.Find(id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Mode = "Edit";
+            return View(technician);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Technicians.Update(technician);
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Mode = "Edit";
+                return View(technician);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var technician = context.Technicians.Find(id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+            return View(technician);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Technician technician)
+        {
+            technician = context.Technicians.Find(technician.TechnicianID);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+            context.Remove(technician);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
