0089aa3eb9227b72f0446c0c78687993c9e3a86c
Kobe Swier <kobe.swier@southeasttech.edu>
2025-01-23 04:03:56 -0600

    Completed CRUD

--- SportsPro/Controllers/IncidentController.cs
+++ SportsPro/Controllers/IncidentController.cs
@@ -1,12 +1,151 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsPro.Controllers
 {
     public class IncidentController : Controller
     {
-        public IActionResult Index()
+        private SportsProContext context { get; set; }
+
+        public IncidentController(SportsProContext ctx)
         {
-            return View();
+            context = ctx;
+        }
+
+        [HttpGet]
+        public ViewResult List()
+        {
+            var incidents = context.Incidents
+            .Include(i => i.Customer) 
+            .Include(i => i.Product) 
+            .OrderBy(i => i.IncidentID)
+            .ToList();
+            return View(incidents);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            var incident = new Incident();
+            ViewBag.Mode = "Add";
+            PopulateDropdowns();
+            return View("Edit", incident);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Incidents.Add(incident);
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Mode = "Add";
+                PopulateDropdowns();
+                return View("Edit", incident);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var incident = context.Incidents.Find(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Mode = "Edit";
+            PopulateDropdowns();
+            return View(incident);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Incidents.Update(incident);
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Mode = "Edit";
+                PopulateDropdowns();
+                return View(incident);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var incident = context.Incidents.Find(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            return View(incident);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Incident incident)
+        {
+            incident = context.Incidents.Find(incident.IncidentID);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            context.Remove(incident);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        private void PopulateDropdowns()
+        {
+            ViewBag.Customers = GetCustomers();
+            ViewBag.Products = GetProducts();
+            ViewBag.Technicians = GetTechnicians();
+        }
+
+        private SelectList GetCustomers()
+        {
+            var customers = context.Customers
+                .Select(c => new
+                {
+                    c.CustomerID,
+                    FullName = c.FirstName + " " + c.LastName 
+                })
+                .OrderBy(c => c.FullName)
+                .ToList();
+
+            return new SelectList(customers, "CustomerID", "FullName");
+        }
+
+        private SelectList GetProducts()
+        {
+            var products = context.Products
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            return new SelectList(products, "ProductID", "Name");
+        }
+
+        private SelectList GetTechnicians()
+        {
+            var technicians = context.Technicians
+                .OrderBy(t => t.Name)
+                .ToList();
+
+            return new SelectList(technicians, "TechnicianID", "Name");
         }
     }
 }
+
+
+
