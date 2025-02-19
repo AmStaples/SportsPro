fbd5501fbcc0bc54f40771775cb5292b74c79d1d
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-19 11:16:47 -0600

    Add server-side validation.

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -1,5 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SportsPro.Models
 {
@@ -7,30 +7,41 @@ namespace SportsPro.Models
     {
 		public int? CustomerID { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Please enter a first name.")]
+		[StringLength(50, ErrorMessage = "First name must be 50 characters or less.")]
 		public string FirstName { get; set; }
 
-		[Required]
-		public string LastName { get; set; }
+		[Required(ErrorMessage = "Please enter a last name.")]
+        [StringLength(50, ErrorMessage = "Last name must be 50 characters or less.")]
+        public string LastName { get; set; }
 
-		[Required]
-		public string Address { get; set; }
+		[Required(ErrorMessage = "Please enter an address.")]
+        [StringLength(50, ErrorMessage = "Address must be 50 characters or less.")]
+        public string Address { get; set; }
 
-		[Required]
-		public string City { get; set; }
+		[Required(ErrorMessage = "Please enter a city.")]
+        [StringLength(50, ErrorMessage = "City must be 50 characters or less.")]
+        public string City { get; set; }
 
-		[Required]
-		public string State { get; set; }
+		[Required(ErrorMessage = "Please enter a state.")]
+        [StringLength(50, ErrorMessage = "State must be 50 characters or less.")]
+        public string State { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Please enter a postal code.")]
+		[StringLength(20, ErrorMessage = "Postal code must be 20 characters or less.")]
 		public string PostalCode { get; set; }
 
-		[Required(ErrorMessage = "The Country field is required.")]
+		[Required(ErrorMessage = "Please select a country.")]
 		public string CountryID { get; set; }
 		public Country Country { get; set; }
 
+		[StringLength(20, ErrorMessage = "Phone number must be 20 characters or less.")]
+		[RegularExpression("^\\(\\d{3}\\) \\d{3}-\\d{4}$", ErrorMessage = "Phone number must be in (999) 999-9999 format.")]
 		public string Phone { get; set; }
 
+		[StringLength(50, ErrorMessage = "Email address must be 50 characters or less.")]
+		[DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address.")]
+		[Remote("CheckEmail", "Customer", AdditionalFields = nameof(CustomerID))]
 		public string Email { get; set; }
 
 		public string FullName => FirstName + " " + LastName;   // read-only property
