aadcf5a00ddc07c8eda5030377e898a65a1714ac
AmStaples <150167944+AmStaples@users.noreply.github.com>
2025-04-23 23:20:51 -0500

    CopyrightInfo component

---
+++ SportsPro/Views/Shared/Components/CopyrightInfo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace SportsPro.Views.Shared.Components
+{
+    public class CopyrightInfo : ViewComponent
+    {
+        private int date = DateTime.Now.Year;
+
+        public IViewComponentResult Invoke() => View("_CopyrightInfo.cshtml", date);
+    }
+}
